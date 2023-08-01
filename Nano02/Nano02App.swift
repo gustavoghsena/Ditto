63a304bd476e7b5bf568ad59ed36ddebd3fc893c
Gustavo Sena <gugasena1999@gmail.com>
2023-08-01 14:09:20 -0300

    Initial Commit

---
+++ Nano02/Nano02App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano02App.swift
+//  Nano02
+//
+//  Created by Gustavo Sena on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Nano02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
